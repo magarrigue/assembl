f8707a2291e26cbf8558f617b7bef557d3bfe56d
Marc-Antoine Parent <maparent@acm.org>
2018-07-19 19:57:08 -0400

    watson task

--- assembl/tasks/__init__.py
+++ assembl/tasks/__init__.py
@@ -133,6 +133,7 @@ class CeleryWithConfig(Celery):
         import assembl.tasks.notify
         import assembl.tasks.notification_dispatch
         import assembl.tasks.translate
+        import assembl.tasks.watson
 
 
 celery = CeleryWithConfig('celery_tasks')
