d6ef10b8b07d8ff9a46fa7077498cd199754ce1f
Pascal Huynh <pascal@beyowi.com>
2018-08-24 12:12:34 +0200

    create fictionHeader components

--- assembl/static2/.storybook/config.js
+++ assembl/static2/.storybook/config.js
@@ -25,6 +25,7 @@ function loadStories() {
   require('../js/app/integration/101/components/checkboxList101/checkboxList101.stories.jsx');
 
   require('../js/app/stories/components/debate/brightMirror/circleAvatar.jsx');
+  require('../js/app/stories/components/debate/brightMirror/fictionHeader.jsx');
 
   // TODO: To clean ?
   // require('../js/app/stories/components/common/common.jsx');
