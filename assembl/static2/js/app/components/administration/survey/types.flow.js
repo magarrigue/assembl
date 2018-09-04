411ae614157b6db48059da6abc14e345429b1fcd
Amen Souissi <amensouissi@ecreall.com>
2018-09-04 23:46:49 +0200

    cleanup, refactoring

--- assembl/static2/js/app/components/administration/survey/types.flow.js
+++ assembl/static2/js/app/components/administration/survey/types.flow.js
@@ -26,7 +26,7 @@ export type ThemeValue = {
   questions: Array<QuestionValue>,
   title: I18nValue,
   video: VideoValue,
-  children: Array<*>
+  children: Array<ThemeValue>
 };
 
 export type ThemesValue = Array<ThemeValue>;
